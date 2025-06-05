4687f31c86f840ed4f7dd2e271b7ace68e485e22
Roshan Kumar <roshankumarrk9570@gmail.com>
2025-06-06 00:09:39 +0530

    add ShimmerCard

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,19 +1,23 @@
 
 import RestaurantCard  from "./RestaurantCard"
+import ShimmerCard from "./ShimmerCard"; 
 
 const Body = ({ restaurants }) => {
+  const isLoading = !restaurants || restaurants.length === 0;
+
   return (
     <div className="restaurant-list">
-      {restaurants?.length > 0 &&
-        restaurants.map((restaurant) => (
-          <RestaurantCard
-            key={restaurant.info.id}
-            {...restaurant.info}
-            lastMileTravelString={restaurant.info.sla.lastMileTravelString}
-          />
-        ))}
+      {isLoading
+        ? Array(10).fill(0).map((_, index) => <ShimmerCard key={index} />)
+        : restaurants.map((restaurant) => (
+            <RestaurantCard
+              key={restaurant.info.id}
+              {...restaurant.info}
+              lastMileTravelString={restaurant.info.sla.lastMileTravelString}
+            />
+          ))}
     </div>
   );
 };
 
-  export default Body;
+export default Body;
---
+++ src/components/ShimmerCard.js
@@ -0,0 +1,16 @@
+const ShimmerCard = () => {
+    return (
+      <div className="shimmer-card">
+        <div className="shimmer-img shimmer"></div>
+        <div className="shimmer-line shimmer"></div>
+        <div className="shimmer-line short shimmer"></div>
+        <div className="shimmer-details">
+          <div className="shimmer-box shimmer"></div>
+          <div className="shimmer-box shimmer"></div>
+          <div className="shimmer-box shimmer"></div>
+        </div>
+      </div>
+    );
+  };
+  
+  export default ShimmerCard;
