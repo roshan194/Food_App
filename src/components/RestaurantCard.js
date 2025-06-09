df78f342ef5bf1d265308adb43205490429de86d
Roshan Kumar <roshankumarrk9570@gmail.com>
2025-06-09 09:59:42 +0530

    truncate text

--- src/components/RestaurantCard.js
+++ src/components/RestaurantCard.js
@@ -15,7 +15,7 @@ const RestaurantCard = ({
       <div className="card">
         <img src={IMG_CDN_URL + cloudinaryImageId} alt="Restaurant" />
         <h2>{name}</h2>
-        <h4>{cuisines.join(", ")}</h4>
+        <h4 className='truncate-text'>{cuisines.join(", ")}</h4>
         <h4>{areaName}</h4>
         <span>
           <h4>
